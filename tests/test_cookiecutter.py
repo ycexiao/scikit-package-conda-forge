8973523d6aa049923be82cd3c117f06223a88192
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-25 22:10:27 +0900

    tests: add tests for generating meta.yaml

---
+++ tests/test_cookiecutter.py
@@ -0,0 +1,30 @@
+import filecmp
+from pathlib import Path
+from cookiecutter.main import cookiecutter
+
+def test_meta_yaml_matches_reference(tmp_path):
+    cookiecutter(
+        ".",
+        no_input=True,
+        extra_context={
+            "github_username_or_orgname": "diffpy",
+            "package_import_name": "diffpy.pdfgui",
+            "github_repo_name": "diffpy.pdfgui",
+            "version": "3.1.0",
+            "min_python_version": "3.11",
+            "project_short_description": "Python package for doing science.",
+            "project_full_description": "This is a Python package for doing science.",
+            "license_file": "LICENSE.rst",
+            "recipe_maintainers": "sbillinge, bobleesj",
+            "build_requirements": "",
+            "host_requirements": "setuptools, setuptools-git-versioning >=2.0, pip",
+            "runtime_requirements": "numpy",
+            "testing_requirements": "pip, pytest"
+        },
+        output_dir=str(tmp_path)
+    )
+    generated = tmp_path / "diffpy.pdfgui" / "meta.yaml"
+    reference = Path("tests/diffpy.example/meta.yaml")
+    assert generated.exists()
+    assert reference.exists()
+    assert filecmp.cmp(generated, reference)
