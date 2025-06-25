9cfd989f4794ffd41464daaa517e7e8f615f3322
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-25 22:21:27 +0900

    chore: fix pre-commit by ignoring jinja variables

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -15,11 +15,11 @@ def test_meta_yaml_matches_reference(tmp_path):
             "version": "3.1.0",
             "min_python_version": "3.11",
             "project_short_description": "Python package for doing science.",
-            "project_full_description": "This is a Python package for doing science.",
+            "project_full_description": "This is a Python package for doing science.",  # noqa: E501
             "license_file": "LICENSE.rst",
             "recipe_maintainers": "sbillinge, bobleesj",
             "build_requirements": "",
-            "host_requirements": "setuptools, setuptools-git-versioning >=2.0, pip",
+            "host_requirements": "setuptools, setuptools-git-versioning >=2.0, pip",  # noqa: E501
             "runtime_requirements": "numpy",
             "testing_requirements": "pip, pytest",
         },
